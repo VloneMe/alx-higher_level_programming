6d47ff499ea1aec4beab9201d4f94235a9ae7139
VloneMe <francisntima@yahoo.com>
2023-06-05 12:59:09 -0700

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list->next;
 
-	if (list == NULL || list->next == NULL)
+	if (!list)
 		return (0);
 
 	while (fast != NULL && fast->next != NULL)
