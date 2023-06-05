3744ea30d75817a56c368dfab707a4607db6cc3c
VloneMe <francisntima@yahoo.com>
2023-06-05 13:09:43 -0700

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,23 +3,25 @@
 /**
  * check_cycle - This function checks if a linked list is circular or not
  * @list: linked list to check
- * Return: 1 (linked list is circular) 0 (no loop detected)
+ *
+ * Return: returns 1 (linked list is circular) 0 (no loop detected)
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list->next;
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
 
-	if (!list)
-		return (0);
-
-	while (fast != NULL && fast->next != NULL)
+	slow = fast = list;
+	while (list && slow && fast && slow->next && fast->next)
 	{
-		if (slow == fast)
-			return (1);
-
 		slow = slow->next;
 		fast = fast->next->next;
+
+		if (!fast || !slow)
+			return (0);
+
+		if (fast->next == slow)
+			return (1);
 	}
 	return (0);
 }
