76f162cbd6dd2c6a7d1856f4aca751948d6aac5a
VloneMe <francisntima@yahoo.com>
2023-06-06 15:54:17 -0700

    Check last number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,8 @@ if lastDigit > 5:
     str = "and is greater than 5"
 elif lastDigit < 6:
     str = "and is less than 6 and not 0"
+elif lastDigit == 0:
+    str = "and is 0"
 else:
     str = "and is 0"
 
