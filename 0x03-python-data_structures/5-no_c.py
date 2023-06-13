2c19939338cc26a9ab1732215245b25cb29fffab
VloneMe <francisntima@yahoo.com>
2023-06-12 18:08:05 -0700

    removed

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-def no_c(my_string):
-    """ function that removes all characters c and C from a string. """
-    y = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(y))
