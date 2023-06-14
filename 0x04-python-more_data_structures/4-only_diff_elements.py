5790dcdf0f5829d29d2f59499c472a408537c5a3
VloneMe <francisntima@yahoo.com>
2023-06-14 08:18:41 -0700

    returns a set of all elements present in only one set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """
+    function that returns a set of all elements present in only one set
+    """
+    diff_set = (set_1 | set_2) - (set_1 & set_2)
+    return diff_set
