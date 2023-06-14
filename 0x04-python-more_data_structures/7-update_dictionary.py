d7c428109339e4e41167f093c2a7459a743bdece
VloneMe <francisntima@yahoo.com>
2023-06-14 08:52:47 -0700

    replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,6 @@ def update_dictionary(a_dictionary, key, value):
     """
     function that replaces or adds key/value in a dictionary
     """
-    updated_dic = dict(a_dictionary)
+    updated_dic = a_dictionary
     updated_dic[key] = value
     return updated_dic
