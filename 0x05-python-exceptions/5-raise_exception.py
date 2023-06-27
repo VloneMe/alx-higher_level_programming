c4e3774be89ce1e28ae0769e577ef9b995b5045d
VloneMe <francisntima@yahoo.com>
2023-06-26 19:34:57 -0700

    raises exeception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """
+    Raises a TypeError exception with a custom error message.
+
+    Raises,
+        TypeError: Always raises a TypeError with the message "Error Occurred".
+    """
+    raise TypeError("Error Occurred")
+
