3e5d9a71184e9ee1a20f723297967a391fee7364
VloneMe <francisntima@yahoo.com>
2023-06-26 19:38:21 -0700

    c is fun

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError exception with a custom error message.
+
+    Raises:
+        NameError: Always raises a NameError with the specified error message.
+    """
+    raise NameError(message)
