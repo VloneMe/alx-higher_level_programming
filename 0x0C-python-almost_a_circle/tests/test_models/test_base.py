b0243da7c2ac1f34607be75e2fb10a3cecd5db61
VloneMe <francisntima@yahoo.com>
2023-07-17 14:46:19 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,9 @@ import unittest
 from algun archivo import algunas clases
 
 class alguna(unittest.TestCase):
+    """
+    Test case class for testing SomeClass1 and SomeClass2.
+    """
     pass
 
 if __name__ == "__main__"":
