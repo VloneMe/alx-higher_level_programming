f14f5918fd5513ed1ebdaa592a23928ea27d6007
VloneMe <francisntima@yahoo.com>
2023-08-15 23:04:02 -0700

    SQL: Always a name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- SQL: script that creates the table force_name on your MySQL server.
+--	If the table force_name already exists, the script won't fail
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
