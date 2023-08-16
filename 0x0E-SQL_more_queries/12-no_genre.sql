63df81d7b968d2bba04d3a9db48cd4c5dddebaae
VloneMe <francisntima@yahoo.com>
2023-08-16 00:32:09 -0700

    SQL: No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- SQL: Script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM hbtn_0d_tvshows.tv_shows
+LEFT JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
