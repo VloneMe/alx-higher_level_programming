ff196a068ae97dba0cc92094974d949d3a1766f1
VloneMe <francisntima@yahoo.com>
2023-08-16 19:39:09 -0700

    SQL: Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- SQL: Script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows.tv_shows
-LEFT JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
