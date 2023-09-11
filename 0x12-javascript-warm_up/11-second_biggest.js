cd32b291b726efe0c291d552845d4b6a7a893761
VloneMe <francisntima@yahoo.com>
2023-09-11 12:37:47 -0700

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,13 @@
 
 // The script that searches the second biggest integer in the list of arguments.
 
-if (process.argv.length <= 3) {
+const args = process.argv.slice(2).map(arg => parseInt(arg));
+
+if (args.length < 2) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  args.sort((a, b) => b - a);
+
+  const secondLargest = args[1];
+  console.log(secondLargest);
 }
