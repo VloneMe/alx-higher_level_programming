8f8296bd75ebdd8a62fe0fb612fd4e3e423c3b60
VloneMe <francisntima@yahoo.com>
2023-09-11 09:48:29 -0700

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,5 +2,10 @@
 
 // This script prints 3 lines.
 
-const myVar = 'C is fun \nPython is cool \nJavaScript is amazing';
+const myVar = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'JavaScript is amazing';
+
 console.log(myVar);
+console.log(myVar1);
+console.log(myVar2);
