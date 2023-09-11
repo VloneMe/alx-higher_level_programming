3544f634a956c13b60955bc3d453791d95b4157d
VloneMe <francisntima@yahoo.com>
2023-09-11 10:29:12 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,10 +4,10 @@
 
 const myVar = process.argv.length - 2;
 
-if (myVar === 0){
-	console.log('No argument');
-} else if ( myVar === 1){
-	console.log('Argument found');
-}else {
-	console.log('Arguments found');
-};
+if (myVar === 0) {
+  console.log('No argument');
+} else if (myVar === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
