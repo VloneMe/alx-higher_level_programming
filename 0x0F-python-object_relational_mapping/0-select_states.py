767763870934a6b2884bdef04fb3d776859c9e67
VloneMe <francisntima@yahoo.com>
2023-09-18 02:56:06 -0700

    lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,14 +5,6 @@ This script that lists all states from the database hbtn_0e_0_usa:
 import sys
 import MySQLdb
 
-"""
-Variables With the Creditials.
-"""
-MY_HOST = "localhost"
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-
 if __name__ == "__main__":
     """
     MySQL Connection
