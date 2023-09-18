9b066c01d459acad46afda2c039ecdb8b29d345d
VloneMe <francisntima@yahoo.com>
2023-09-18 09:41:07 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,18 @@
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/python3
+"""
+This script contains State class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+_metadata = MetaData()
+Base = declarative_base(metadata=_metadata)
 
 class State(Base):
+    """
+    Table states
+    """
+
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
