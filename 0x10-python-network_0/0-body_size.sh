4db7dbfff33fe9e387cd0be7453bf8de73be2e6e
VloneMe <francisntima@yahoo.com>
2023-09-28 18:49:01 -0700

    curl res

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-# A bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-curl -so /dev/null "$1" -w '%{size_download}\n'
+#Sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep Content-Length: | cut -d " " -f 2
