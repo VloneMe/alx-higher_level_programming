7b6da94418921b76b7132f4897483d37b0b15bc8
VloneMe <francisntima@yahoo.com>
2023-11-02 21:58:57 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,15 +6,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-if (!filePath || !contentToWrite) {
-  console.error('Please provide a file path and content as arguments');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('File content has been written successfully.');
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
   }
 });
