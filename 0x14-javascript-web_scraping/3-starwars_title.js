ce66da75b78c66b241199af81215f965eef28b30
VloneMe <francisntima@yahoo.com>
2023-11-02 22:11:46 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,23 +3,13 @@
 // where the episode number matches a given integer.
 
 const request = require('request');
-
 const movieId = process.argv[2];
+const url = 'http://swapi.co/api/films/' + movieId;
 
-if (!movieId) {
-  console.error('Please provide a movie ID as an argument');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(apiUrl, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
-    console.error('Error:', error.message);
-  } else if (response.statusCode !== 200) {
-    console.error(`Request failed with status code: ${response.statusCode}`);
+    console.log(error);
   } else {
-    const movie = JSON.parse(body);
-    console.log('Title:', movie.title);
+    console.log(JSON.parse(body).title);
   }
 });
