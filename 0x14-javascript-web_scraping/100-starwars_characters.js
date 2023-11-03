10dd83738b1729cd6f01cac1797be05b770a44a0
VloneMe <francisntima@yahoo.com>
2023-11-03 00:24:44 -0700

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // A script that gets the contents of a webpage and stores it in a file.
 
-const url = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/'
 const filePath = process.argv[3];
 
 const request = require('request');
