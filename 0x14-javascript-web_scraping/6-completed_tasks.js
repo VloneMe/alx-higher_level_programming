0d81177e2fb4616339665be819790e9213a753db
VloneMe <francisntima@yahoo.com>
2023-11-02 22:59:38 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,17 +4,24 @@
 const request = require('request');
 let tasks = {};
 
-request(process.argv[2], function (error, response, body) {
+const apiUrl = process.argv[2]; // Get the API URL from the command line arguments
+
+if (!apiUrl) {
+  console.error('Please provide the API URL as a command line argument');
+  process.exit(1);
+}
+
+request(apiUrl, function (error, response, body) {
   if (error) {
     throw (error);
   } else {
-    let taskList = JSON.parse(response.body);
-    for (let items in taskList) {
-      if (!(tasks[taskList[items]['userId']]) && taskList[items]['completed'] === true) {
-        tasks[taskList[items]['userId']] = 0;
+    let taskList = JSON.parse(body); // Use 'body' directly instead of 'response.body'
+    for (let item of taskList) { // Use 'item' instead of 'items'
+      if (!(tasks[item['userId']]) && item['completed'] === true) {
+        tasks[item['userId']] = 0;
       }
-      if (taskList[items]['completed'] === true) {
-        tasks[taskList[items]['userId']] += 1;
+      if (item['completed'] === true) {
+        tasks[item['userId']] += 1;
       }
     }
     console.log(tasks);
