f95d7a0714233303d5bc4db0b074629cd3e5940a
VloneMe <francisntima@yahoo.com>
2023-11-02 23:52:25 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,9 +6,10 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = 'http://swapi.co/api/films/' + movieId;
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log('code: ' + response.statusCode);
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-  console.log(JSON.parse(body)['title']);
 });
