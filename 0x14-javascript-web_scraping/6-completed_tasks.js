978c0780e23bca2cec87f828a02883cf84ed4be3
VloneMe <francisntima@yahoo.com>
2023-11-02 22:51:49 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // A script that computes the number of tasks completed by user id.
 
-const request = require('request');
 const apiUrl = process.argv[2];
-
-const fs = require('fs');
 const filePath = process.argv[3];
+
+const request = require('request');
+const fs = require('fs');
 
 request(apiUrl, function (error, response, body) {
   if (error) {
