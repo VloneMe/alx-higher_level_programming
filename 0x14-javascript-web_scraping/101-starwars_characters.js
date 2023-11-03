f7dc8667e5b1eb48c4f46fee969d9472537a8ad2
VloneMe <francisntima@yahoo.com>
2023-11-03 01:08:44 -0700

    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,44 @@
 #!/usr/bin/node
-// A script that prints all characters of a Star Wars movie:
 
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-const file = process.argv[3];
-const req = require('request');
-const fs = require('fs');
+const request = require('request');
+const movieId = process.argv[2];
 
-req(apiUrl, function (error, response, body) {
+if (!movieId) {
+  console.error('Please provide the Movie ID as a command line argument');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Request failed with status code:', response.statusCode);
   } else {
-    fs.writeFile(file, body, 'utf-8', (error) => {
-      if (error) console.log(error);
-    });
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
+    
+    // Function to fetch and print character names
+    function fetchCharacter(index) {
+      if (index < characterUrls.length) {
+        request(characterUrls[index], function (error, response, characterBody) {
+          if (error) {
+            console.error('Error fetching character:', error);
+          } else if (response.statusCode !== 200) {
+            console.error('Request failed with status code:', response.statusCode);
+          } else {
+            const characterData = JSON.parse(characterBody);
+            console.log(characterData.name);
+          }
+          // Fetch the next character
+          fetchCharacter(index + 1);
+        });
+      }
+    }
+
+    console.log(`Characters in ${movieData.title}:`);
+    // Start fetching characters from index 0
+    fetchCharacter(0);
   }
 });
