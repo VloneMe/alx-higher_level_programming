880341b8923957af157d3d061c03eaa58d4687eb
VloneMe <francisntima@yahoo.com>
2023-11-02 22:05:43 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
 // A script that display the status code of a GET request.
 
-const request = require('request-promise-native');
+const request = require('request');
 
 const url = process.argv[2];
 
-if (!url) {
-  console.error('Please provide a URL as an argument');
-  process.exit(1);
-}
-
-request.get(url)
-  .then((response) => {
-    // Parse the response and display the status code
-    console.log(`code: ${response.statusCode}`);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-  });
+request(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
