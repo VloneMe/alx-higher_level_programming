aad075a085402b9dc698d62359ffad2baa612001
VloneMe <francisntima@yahoo.com>
2023-11-03 20:33:34 -0700

    Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+// JavaScript script that fetches and lists the title for all movies
+// by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json
+
+$(document).ready(function() {
+    $.get("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
+        const movies = data.results;
+        const ulElement = $("#list_movies");
+        movies.forEach(function(movie) {
+            const liElement = $("<li>").text(movie.title);
+            ulElement.append(liElement);
+        });
+    });
+}
