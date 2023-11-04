9c35cb9f64d42b3175908386035b85ed50049189
VloneMe <francisntima@yahoo.com>
2023-11-03 19:52:01 -0700

    Toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,14 @@
+// JavaScript script that toggles the class of the <header> element
+// when the user clicks on the tag DIV#toggle_header:
+
+$(document).ready(function() {
+	$("#toggle_header").click(function() {
+		const header = $("header");
+
+                if (header.hasClass("red")) {
+                    header.removeClass("red").addClass("green");
+                } else {
+                    header.removeClass("green").addClass("red");
+                }
+        });
+});
