877be82cb1433882d8fc0218b421f0bd25712ee1
VloneMe <francisntima@yahoo.com>
2023-11-03 20:06:42 -0700

    Change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+// JavaScript script that updates the text of the <header> element
+// to New Header!!! when the user clicks on DIV#update_header.
+
+$(document).ready(function() {
+	$("#update_header").click(function() {
+                $("header h1").text("New Header!!!");
+        });
+});
