c03b75e46a02bb0b3e9d7076bc8e949a9e20fe17
VloneMe <francisntima@yahoo.com>
2023-11-03 19:58:59 -0700

    List of elements

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,10 @@
+// JavaScript script that adds a <li> element to a list
+// when the user clicks on the tag DIV#add_item:
+
+$(document).ready(function() {
+	$("#add_item").click(function() {
+                const newItem = $("<li>Item</li>");
+
+                $(".my_list").append(newItem);
+        });
+});
