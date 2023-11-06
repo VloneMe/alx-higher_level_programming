c61134984ebdceaac0d8d348ca26fe802efd3b5f
VloneMe <francisntima@yahoo.com>
2023-11-05 20:43:35 -0800

    And press ENTER

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,21 @@
+// JavaScript script that fetches and prints
+// how to say “Hello” depending on the language
+
+$(document).ready(function() {
+
+    function fetchTranslation() {
+        const languageCode = $("#language_code").val();
+
+        $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function(data) {
+            $("#hello").text(data.hello);
+        });
+    }
+
+    $("#btn_translate").click(fetchTranslation);
+
+    $("#language_code").keypress(function(event) {
+        if (event.which === 13) {
+            fetchTranslation();
+        }
+    });
+});
