01037c42e5a4993ac6fbbd88e33120607e95d362
VloneMe <francisntima@yahoo.com>
2023-11-05 20:33:41 -0800

    Say hello to everybody!

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,14 @@
+// JavaScript script that fetches and prints
+// how to say “Hello” depending on the language
+
+$(document).ready(function() {
+
+    $("#btn_translate").click(function() {
+        const languageCode = $("#language_code").val();
+
+
+        $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function(data) {
+            $("#hello").text(data.hello);
+        });
+    });
+});
